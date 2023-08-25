4990b6069513e3114864cd989beda13b1705fcb4
Jake Sadowski <110630130+jsadowski7@users.noreply.github.com>
2023-08-24 19:06:28 -0500

    Created react app

---
+++ onepager-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import Card  from "./components/card.component";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Card title={"Hello"}></Card>
+      <Card description={"World"}></Card>
+    </div>
+  );
+}
+
+export default App;
